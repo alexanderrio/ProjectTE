f175f39fbb935c49bc722d2c5a39cd09e0385588
kristofanf <44857308+kristofanf@users.noreply.github.com>
2021-06-01 10:40:25 +0700

    Update EditUserGagal

--- Scripts/editUserGagal/Script1622515565563.groovy
+++ Scripts/editUserGagal/Script1622515565563.groovy
@@ -17,3 +17,63 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://opensource-demo.orangehrmlive.com/')
+
+WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/span_Username'))
+
+WebUI.setText(findTestObject('Object Repository/Page_OrangeHRM/input_LOGIN Panel_txtUsername'), 'admin')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_OrangeHRM/input_Username_txtPassword'), 'hUKwJTbofgPU9eVlw/CnDQ==')
+
+WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/input_Password_Submit'))
+
+WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/b_Admin'))
+
+WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/a_123456'))
+
+WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/input__btnSave'))
+
+WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/li_Username'))
+
+WebUI.setText(findTestObject('Object Repository/Page_OrangeHRM/input__systemUseruserName'), '1234')
+
+WebUI.verifyElementVisible(findTestObject('Object Repository/Page_OrangeHRM/span_Should have at least 5 characters'))
+
+WebUI.setText(findTestObject('Object Repository/Page_OrangeHRM/input__systemUseremployeeNameempName'), 'test')
+
+WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/li_Employee Name   Employee does not exist _a54bdb'))
+
+WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/input_Change Password_systemUserchkChangePassword'))
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_OrangeHRM/input__systemUserpassword'), 'gI2mlAjwl1k=')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_OrangeHRM/input__systemUserconfirmPassword'), 'gI2mlAjwl1k=')
+
+WebUI.verifyElementVisible(findTestObject('Object Repository/Page_OrangeHRM/span_Should have at least 8 characters'))
+
+WebUI.doubleClick(findTestObject('Object Repository/Page_OrangeHRM/input__systemUseremployeeNameempName'))
+
+WebUI.setText(findTestObject('Object Repository/Page_OrangeHRM/input__systemUseremployeeNameempName'), 'a')
+
+WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/li_Aaliyah Haq'))
+
+WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/input__systemUseruserName'))
+
+WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/input__btnSave'))
+
+WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/input__btnSave'))
+
+WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/a_Welcome Paul'))
+
+
+WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/a_Logout'))
+
+WebUI.setText(findTestObject('Object Repository/Page_OrangeHRM/input_LOGIN Panel_txtUsername'), '1234')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_OrangeHRM/input_Username_txtPassword'), 'gI2mlAjwl1k=')
+
+WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/input_Password_Submit'))
+
+WebUI.closeBrowser()
